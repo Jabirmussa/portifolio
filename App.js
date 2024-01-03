428732550dba496ec4883e2765c30f23f09c8ee1
jabirmussa <jabirmussa185@gmail.com>
2024-01-03 07:08:01 +0200

    alot

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 
+const interObserv = new IntersectionObserver((entries) =>{
+    entries.forEach(entry =>{
+    console.log(entry)
+    entry.target.classList.toggle("show", entry.isIntersecting)
+    })
+})
+const elements = document.querySelectorAll(".animation")
+elements.forEach(el =>
+    IntersectObserver.observe(el)
+)
+
 function showMoreImages() {
+
   var hiddenImages = document.querySelectorAll('.img1.hidden');
-  
   hiddenImages.forEach(function (image) {
       image.classList.toggle('hidden');
   });
   var verMaisBtn = document.getElementById('ver-mais-btn');
   var linkText = verMaisBtn.querySelector('a');
-
   if (linkText.textContent === 'Ver mais...') {
       linkText.textContent = 'Ver menos...';
-  } else {
+  } 
+  else {
       linkText.textContent = 'Ver mais...';
   }
+  
 }
