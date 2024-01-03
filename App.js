c0b41d2b672e620ede8d9268e8ca2352df5e08fe
jabirmussa <jabirmussa185@gmail.com>
2024-01-03 07:49:41 +0200

    just

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 
-const interObserv = new IntersectionObserver((entries) =>{
-    entries.forEach(entry =>{
-    console.log(entry)
-    entry.target.classList.toggle("show", entry.isIntersecting)
-    })
-})
-const elements = document.querySelectorAll(".animation")
-elements.forEach(el =>
-    IntersectObserver.observe(el)
-)
-
 function showMoreImages() {
-
+    
   var hiddenImages = document.querySelectorAll('.img1.hidden');
   hiddenImages.forEach(function (image) {
       image.classList.toggle('hidden');
-  });
+    });
   var verMaisBtn = document.getElementById('ver-mais-btn');
   var linkText = verMaisBtn.querySelector('a');
   if (linkText.textContent === 'Ver mais...') {
@@ -23,6 +12,14 @@ function showMoreImages() {
   } 
   else {
       linkText.textContent = 'Ver mais...';
-  }
-  
+    }
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+    console.log(entry)
+    entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+});
+const elements = document.querySelectorAll(".animation")
+elements.forEach(el => observer.observe(el))
